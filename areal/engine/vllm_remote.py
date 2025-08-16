4d7db41c96c7d83696de5fa46457edc0cb45ac6f
ronbogo <ronbogo@outlook.com>
2025-08-16 17:10:39 +0800

    fix stop reason

--- areal/engine/vllm_remote.py
+++ areal/engine/vllm_remote.py
@@ -169,7 +169,7 @@ class RemotevLLMEngine(InferenceEngine):
             meta_info = result["choices"][0]
             # Check if generation is complete
             finish_reason = meta_info["finish_reason"]
-            stop_reason = meta_info["stop_reason"]
+            stop_reason = finish_reason
             if (
                 stop_reason == "abort"
                 and finish_reason.get("message") == "Abort before prefill"
